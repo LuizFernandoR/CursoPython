25fe3253a31ba082fcc275c51e1a3aefc361fe4d
Luiz Fernando Reichert <67746@proway.treina>
2020-06-20 09:59:49 -0300

    exrcicio

---
+++ Python-aula03/Exercicio01.py
@@ -0,0 +1,45 @@
+# nota1= float(input('Digite a primeira nota: '))
+# nota2=float(input('Digite a segunda nota: '))
+# nota3=float(input('Digite a terceira nota: '))
+# nota4=float(input('Digite a quarta nota: '))
+
+# media= (nota1 + nota2 + nota3 + nota4) /4
+# if media >= 7:
+#     print('Aprovado')
+# elif media >= 5.5:
+#     print ('Recuperação')
+# else:
+#     print('Reprovado')
+
+# resultado = 0
+# for i in range (30):
+#     nota= float(input('Digite a nota'))
+#     resultado= resultado + nota
+# media = resultado/30
+
+# if media >= 7:
+#     print('Aprovado',media)
+# elif media >= 5.5:
+#     print ('Recuperação',media)
+# else:
+#     print('Reprovado',media)
+
+# Control + k + c Descomentar
+
+lista = []
+for i in range(3):
+     nota= float(input(f'Digite a nota{i+1}: '))
+     lista.append(nota)    
+# ac = 0
+# for i in lista:
+#     ac= ac +  1
+media = sum(lista)/len(lista)
+
+if media >= 7:
+    print('Aprovado',media)
+elif media >= 5.5:
+    print ('Recuperação',media)
+else:
+    print('Reprovado',media)
+
+
---
+++ Python-aula03/controle_de_fluxo.py
@@ -0,0 +1,20 @@
+# Um pouco sobre o while
+# a= 0
+# while a < 10:
+#     print(a)
+#     a= a+1
+# if True:
+#     pass
+
+# operadores de comparação
+# Retorna verdadeiro  ou falso ('True','False')
+
+# Igual ==
+# Diferente !=
+# Maior >
+# Menor <
+# Maior igual >=
+# Mennor igual <=
+
+
+
