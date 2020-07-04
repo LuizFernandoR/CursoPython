c031f865bb89f537f413f7fedd522f1f7d34446f
Luiz fernando Reichert <nandoreichert1506@gmail.com>
2020-07-04 19:50:29 -0300

    Praticas

---
+++ Praticando/Praticas.py
@@ -0,0 +1,24 @@
+# lista = [1,2,3,4,5]
+# lista2 = [6,7,8,9,10]
+
+# junta_lista = list(zip(lista,lista2))
+# print(junta_lista)
+
+# print(type(False))
+
+# lista = [1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20]
+# lista2 = [x for x in lista if x == 12 or x == 25]
+# print(lista,lista2)
+
+#arquivo = open ('Python-Aula04/Cadastro.txt','r')
+# lista =['id','nome','idade','sexo','email','telefone']
+# lista_cadastro= []
+# for pessoa in arquivo:
+#     pessoa = pessoa.strip()
+#     pessoa = pessoa.split(';')
+#     lista_cadastro.append(pessoa)
+#     cadastro = dict(zip(lista,lista_cadastro))
+# print(cadastro)
+
+
+
