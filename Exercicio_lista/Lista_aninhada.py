ccabf87806cb5bca9d97d0f7999c5c66ac66cbc4
Luiz fernando Reichert <nandoreichert1506@gmail.com>
2020-07-08 21:46:49 -0300

    Exercícios-lista

---
+++ Exercicio_lista/Lista_aninhada.py
@@ -0,0 +1,41 @@
+l = [
+          ['frutas','verduras','legumes'],
+          ['mamão','abacaxi','laranja','uva','pera','maçã','vergamota'],
+          ['alface crespa', 'alface lisa','rucula','almerão','repolho','salsinha','cebolinha'],
+          ['feijão', 'erviha', 'lentilha','vagem','feijão branco','grão de bico','soja']
+        ]
+# Exercício 01
+print('\n',l[0][0])
+print(l[1],'\n')
+print(l[0][1])
+print(l[2],'\n')
+print(l[0][2])
+print(l[3])
+
+#Exercício 02 
+print(f'{l[1][1]} - {l[1][2]} - {l[1][5]} - {l[1][6]}')
+
+# Exercício 03
+print(f'{l[2][1]} - {l[2][5]} - {l[2][2]} - {l[2][0]}')
+
+# Exercício 04
+print(f'{l[0][0]}: {l[1][2]} e {l[1][4]}','\n')
+print(f'{l[0][1]}: {l[2][4]} e {l[2][2]}','\n')
+print(f'{l[0][2]}: {l[3][1]}, {l[3][4]} e {l[3][5]}')
+
+# Exercício 05
+print(f'{l[1][0]} - {l[3][1]} - {l[2][2]} - {l[2][5]} - {l[1][0]} - {l[2][4]}')
+
+# Exercício 06
+print(f'{l[0][0]}: 3kg de {l[1][2]}, 8kg de {l[1][3]}, 4.5kg de {l[1][5]}, 1kg de {l[1][6]}')
+
+# Exercício 07
+print(f'{l[0][1]}: 5 maço de {l[2][5]}, 4 pés de {l[2][0]} e {l[2][1]}, 2 cabeças de {l[2][4]}')
+
+# Exercício 08
+print(f'{l[0][2]}: 1 kg de {l[3][0]}, 2kg de {l[3][5]}, 1.5kg de {l[3][6]}, 1 pacote de {l[3][1]}')
+
+# Exercício 09
+print(f'{l[0][2]}:{l[3]}')
+
+
---
+++ Exercicio_lista/Lista_aninhada02.py
@@ -0,0 +1,42 @@
+l = [['frutas','verduras','legumes','preço'],
+         ['mamão','abacaxi','laranja','uva','pera','maçã','vergamota'],
+         ['alface crespa', 'alface lisa','rucula','almerão','repolho','salsinha','cebolinha'],
+         ['feijão', 'erviha', 'lentilha','vagem','feijão branco','gão de bico','soja'],
+         [ [10.00, 2.56, 5.25, 9.5, 10.05, 15, 5.75], [2.99, 2.95, 3.5, 3.25, 5.89, 2.9, 2.5],
+           [9.0, 5.0, 7.5, 1.75, 10.9, 5.99, 3.55] 
+         ]
+        ]
+# Exercício01
+print(f'O preço do {l[1][1]} é de R$: {l[4][0][1]:3.2f}')
+# Exercício02
+print(f'O preço da {l[2][2]} é de R$: {l[4][1][2]:3.2f}')
+# Exercício03
+print(f'O preço da {l[1][2]} é de R$: {l[4][0][2]:3.2f}')
+# Exercício04
+print(f'O preço do {l[2][4]} é de R$: {l[4][1][4]:3.2f}')
+# Exercício05
+print(f'O preço do {l[3][0]} é de R$: {l[4][2][0]:3.2f}')
+# Exercício06
+print(f'O preço do {l[3][4]} é de R$: {l[4][2][4]:3.2f}')
+# Exercício07
+print(f'O preço da {l[1][6]} é de R$: {l[4][0][6]:3.2f}')
+# Exercício08
+print(f'O preço do {l[2][1]} é de R$: {l[4][1][1]:3.2f}')
+# Exercício09
+print(f'O preço do {l[1][0]} é de R$: {l[4][0][0]:3.2f}')
+# Exercício10
+print(f'O preço da {l[3][6]} é de R$: {l[4][2][6]:3.2f}')
+# Exercício11
+print(f'O preço da {l[3][2]} é de R$: {l[4][2][2]:3.2f}')
+# Exercício12
+print(f'O preço da {l[1][3]} é de R$: {l[4][0][3]:3.2f}')
+# Exercício13
+print(f'O preço da {l[3][3]} é de R$: {l[4][2][3]:3.2f}')
+# Exercício14
+print(f'O preço da {l[2][3]} é de R$: {l[4][1][3]:3.2f}')
+# Exercício15
+print(f'O preço da {l[3][1]} é de R$: {l[4][2][1]:3.2f}')
+# Exercício16
+print(f'O preço da {l[1][5]} é de R$: {l[4][0][5]:3.2f}')
+
+
